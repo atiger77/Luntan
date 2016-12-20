bc45514e7288ba1938ceb017b8c43cafe32ee5b3
Atiger77 <atiger77@126.com>
2016-12-20 21:46:56 +0800

    Completed ArticlView

--- Article/models.py
+++ Article/models.py
@@ -6,8 +6,8 @@ from Block.models import Block
 # Create your models here.
 class Article(models.Model):
     block = models.ForeignKey(Block,verbose_name="板块ID")
-    title = models.CharField(u"板块名称",max_length=100)
-    content =  models.CharField(u"板块描述",max_length=500)
+    title = models.CharField(u"文章名称",max_length=100)
+    content =  models.CharField(u"文章描述",max_length=500)
     status = models.IntegerField(u"状态",choices=((0,"正常"),(1,"删除")))
 
     create_timestamp = models.DateTimeField('创建时间',auto_now_add=True)
--- Article/views.py
+++ Article/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from Block.models import Block
+from Article.models import Article
 # Create your views here.
 
 def article_list(request,block_id):
     block_id = int(block_id)
-    #返回页面
-    return render(request,"article_list.html")
+    block = Block.objects.get(id=block_id)
+    article_objs = Article.objects.filter(block=block,status=0).order_by("-id")
+    return render(request,"article_list.html",{"articles":article_objs,"b":block})
