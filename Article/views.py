30ce04a02d57b579d37b673f88f21f264f24ac85
Atiger77 <atiger77@126.com>
2016-12-20 22:24:34 +0800

    add article-addfuction

--- Article/views.py
+++ Article/views.py
@@ -8,3 +8,8 @@ def article_list(request,block_id):
     block = Block.objects.get(id=block_id)
     article_objs = Article.objects.filter(block=block,status=0).order_by("-id")
     return render(request,"article_list.html",{"articles":article_objs,"b":block})
+
+def article_add(request,block_id):
+    block_id = int(block_id)
+    block = Block.objects.get(id=block_id)
+    return render(request,"article_add.html",{"b":block})
