b0687239af16692e25f9f5e36178fdbaf6f20ac9
Atiger77 <atiger77@126.com>
2017-01-02 23:05:15 +0800

    Add article_owner

--- Article/models.py
+++ Article/models.py
@@ -1,13 +1,16 @@
+# coding=utf-8
 from __future__ import unicode_literals
 
 from django.db import models
 from Block.models import Block
+from django.contrib.auth.models import  User
 
 # Create your models here.
 class Article(models.Model):
+    owner = models.ForeignKey(User,verbose_name="作者")
     block = models.ForeignKey(Block,verbose_name="板块ID")
     title = models.CharField(u"文章名称",max_length=100)
-    content =  models.CharField(u"文章描述",max_length=500)
+    content = models.CharField(u"文章描述",max_length=500)
     status = models.IntegerField(u"状态",choices=((0,"正常"),(1,"删除")))
 
     create_timestamp = models.DateTimeField('创建时间',auto_now_add=True)
